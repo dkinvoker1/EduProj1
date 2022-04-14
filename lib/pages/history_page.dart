bfad57b4f2b80d366e29377f85251b91f89ae9e6
spajak <spajak@evertop.pl>
2022-04-14 13:07:56 +0200

    historia liczba

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,3 +1,5 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:edu_proj1/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -11,30 +13,56 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
-  var _resultsStream =
-      FirebaseFirestore.instance.collection('results').snapshots();
+  var _resultsStream = FirebaseFirestore.instance
+      .collection('results')
+      .withConverter<Result>(
+        fromFirestore: (snapshot, _) => Result.fromJson(snapshot.data()!),
+        toFirestore: (movie, _) => movie.toJson(),
+      )
+      .snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder(
           stream: _resultsStream,
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            return snapshot.connectionState == ConnectionState.waiting
-                ? Text("Waiting for data")
-                : ListView(
-                    children:
-                        snapshot.data!.docs.map((DocumentSnapshot document) {
-                      Map<String, dynamic> data =
-                          document.data()! as Map<String, dynamic>;
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Result>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Waiting for data");
+            }
 
-                      var a = Result.fromJson(data);
-                      return ListTile(
-                        title: Text(a.isHeven ? "Heven" : "Hell"),
-                      );
-                    }).toList(),
-                  );
+            var _hevenCount = snapshot.data!.docs
+                .where((element) => element.data().isHeven)
+                .length;
+
+            var _hellCount = snapshot.data!.docs
+                .where((element) => !element.data().isHeven)
+                .length;
+
+            return Center(
+              child: Column(
+                children: [
+                  Expanded(
+                      child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("Heven: " + _hevenCount.toString()))),
+                  Expanded(
+                      child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text("Hell: " + _hellCount.toString()))),
+                  Expanded(
+                      child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: IconButton(
+                        onPressed: () async {
+                          context.router.push(MyHomeRoute());
+                        },
+                        icon: Icon(Icons.arrow_back_rounded)),
+                  ))
+                ],
+              ),
+            );
           }),
     );
   }
