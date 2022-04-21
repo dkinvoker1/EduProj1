3c5e970a9ca05f27069e93f474469c715126b597
spajak <spajak@evertop.pl>
2022-04-21 12:40:37 +0200

    refactor

--- lib/routes/authGuard.dart
+++ lib/routes/authGuard.dart
@@ -5,15 +5,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthGuard extends AutoRouteGuard {
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) {
-    // the navigation is paused until resolver.next() is called with either
-    // true to resume/continue navigation or false to abort navigation
-
-    var aaaa = FirebaseAuth.instance.currentUser;
-    if (aaaa != null) {
-      // if user is authenticated we continue
+    var _currentUser = FirebaseAuth.instance.currentUser;
+    if (_currentUser != null) {
       resolver.next(true);
     } else {
-      // we redirect the user to our login page
       router.push(LoginRoute());
     }
   }
